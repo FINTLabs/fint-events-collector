4eacdf1fd7fc362c7603fa4d117076c0ee8dc49b
Asgeir Nilsen <asgeir@fintlabs.no>
2020-03-13 15:55:40 +0100

    Args didn't work.

--- src/main/java/no/fint/eventscollector/Collector.java
+++ src/main/java/no/fint/eventscollector/Collector.java
@@ -2,6 +2,7 @@ package no.fint.eventscollector;
 
 import lombok.extern.slf4j.Slf4j;
 import no.fint.audit.model.AuditEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,22 @@ import java.util.List;
 public class Collector implements ApplicationRunner {
     private final EventsService eventsService;
     private final BlobStorage blobStorage;
+    private final String[] orgids;
 
-    public Collector(EventsService eventsService, BlobStorage blobStorage) {
+    public Collector(
+            EventsService eventsService,
+            BlobStorage blobStorage,
+            @Value("${fint.audit.orgids}") String[] orgids
+            ) {
         this.eventsService = eventsService;
         this.blobStorage = blobStorage;
+        this.orgids = orgids;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        for (String orgId : args.getNonOptionArgs()) {
+        for (int i = 0; i < orgids.length; i++) {
+            String orgId = orgids[i];
             List<AuditEvent> events = eventsService.getAuditEvents(orgId, "24h");
             blobStorage.storeEvents(orgId, events);
             log.info("Stored {} events for {}", events.size(), orgId);
